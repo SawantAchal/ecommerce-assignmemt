e525c4a057dd5e77cfcd8d67f2186f84a71a6af4
SawantAchal <achals1718@gmail.com>
2024-03-13 14:31:46 +0530

    data for mainCarousel

---
+++ src/data/MainCarouselData.js
@@ -0,0 +1,18 @@
+export const MainCarouselData  = [
+    {
+        image : "https://img.freepik.com/free-vector/horizontal-sale-banner-template_23-2148897328.jpg?w=1380&t=st=1709208906~exp=1709209506~hmac=f7ed4699fc36f108898b916d1aa4434f630934389c6b391d57a6cefc1e7d1c0e",
+        path : ""
+    },
+    {
+        image : "https://img.freepik.com/free-psd/horizontal-banner-online-fashion-sale_23-2148585404.jpg?w=1380&t=st=1709209134~exp=1709209734~hmac=0e762a7cb0caf49dbc8f28a15e56ed6cc37806beba95b5d602ca70b5c3a99d6f",
+        path : ""
+    },
+    {
+        image : "https://img.freepik.com/free-vector/abstract-sales-landing-page-with-photo_52683-28047.jpg?w=1060&t=st=1709209162~exp=1709209762~hmac=288208db717dcc9510d1449b26b12407f5b272eef08daa82f9c53097896859a1",
+        path : ""
+    },
+    {
+        image : "https://img.freepik.com/free-psd/sales-template-design-banner_23-2149174599.jpg?w=1380&t=st=1709209179~exp=1709209779~hmac=2f28a50d5cbc6d8a0186c54fa6fa961cd23b41e9e6e05b045bf7e3400de2aee0",
+        path : ""
+    },
+]
