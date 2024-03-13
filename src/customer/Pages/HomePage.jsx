93d62cdbd8a4a9055042b64564db98c2959aebdf
SawantAchal <achals1718@gmail.com>
2024-03-13 14:30:59 +0530

    created a page to display all component

---
+++ src/customer/Pages/HomePage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import MainCarousel from '../component/homeCarousel/MainCarousel'
+import HomeSectionCarousel from '../component/homeSectionCarousel/HomeSectionCarousel'
+import { mens_kurta } from '../../data/Mens_kurta'
+
+const HomePage = () => {
+  return (
+    <div>
+        <MainCarousel/>
+        <div className='space-y-10 py-20 flex flex-col'>
+        <HomeSectionCarousel data = {mens_kurta} sectionName={`men's Kurta`}/>
+        <HomeSectionCarousel data = {mens_kurta} sectionName={`men's Shoes`}/>
+        <HomeSectionCarousel data = {mens_kurta}/>
+        <HomeSectionCarousel data = {mens_kurta}/>
+        </div>
+    </div>
+  )
+}
+
+export default HomePage
