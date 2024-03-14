855efb3b57a45108888676c396f4a38d475772de
SawantAchal <achals1718@gmail.com>
2024-03-14 21:25:25 +0530

    edit existing code

--- src/customer/component/navbar/Navbar.jsx
+++ src/customer/component/navbar/Navbar.jsx
@@ -15,6 +15,8 @@
 import { Fragment, useState } from 'react'
 import { Dialog, Popover, Tab, Transition } from '@headlessui/react'
 import { Bars3Icon, MagnifyingGlassIcon, ShoppingBagIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Avatar, Button, Menu, MenuItem } from '@mui/material'
+import { deepPurple } from '@mui/material/colors'
 
 const navigation = {
   categories: [
@@ -145,41 +147,45 @@ function classNames(...classes) {
 
 export default function Navbar() {
   const [open, setOpen] = useState(false)
+  const [openAuthModel , setOpenAuthModal] = useState(false);
+  const [anchorEl , setAnchorEl] = useState(null)
+  const openUserMenu = Boolean(anchorEl)
+  const jwt = localStorage.getItem("jwt")
+
+  const handleUserClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleCloseUserMenu = (event) => {
+    setAnchorEl(null)
+  }
+
+  const handleOpen = () => {
+    setOpenAuthModal(true)
+  }
+
+  const handleClose = () => {
+    setOpenAuthModal(false)
+  }
+
+  const handleCategoryClick = (category , section , item , close) => {
+    close()
+  }
 
   return (
-    <div className="bg-white">
+    <div className="bg-white z-50">
       {/* Mobile menu */}
       <Transition.Root show={open} as={Fragment}>
         <Dialog as="div" className="relative z-40 lg:hidden" onClose={setOpen}>
-          <Transition.Child
-            as={Fragment}
-            enter="transition-opacity ease-linear duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="transition-opacity ease-linear duration-300"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
+          <Transition.Child as={Fragment} enter="transition-opacity ease-linear duration-300" enterFrom="opacity-0" enterTo="opacity-100" leave="transition-opacity ease-linear duration-300" leaveFrom="opacity-100" leaveTo="opacity-0" >
             <div className="fixed inset-0 bg-black bg-opacity-25" />
           </Transition.Child>
 
           <div className="fixed inset-0 z-40 flex">
-            <Transition.Child
-              as={Fragment}
-              enter="transition ease-in-out duration-300 transform"
-              enterFrom="-translate-x-full"
-              enterTo="translate-x-0"
-              leave="transition ease-in-out duration-300 transform"
-              leaveFrom="translate-x-0"
-              leaveTo="-translate-x-full"
-            >
+            <Transition.Child as={Fragment} enter="transition ease-in-out duration-300 transform" enterFrom="-translate-x-full" enterTo="translate-x-0" leave="transition ease-in-out duration-300 transform" leaveFrom="translate-x-0" leaveTo="-translate-x-full" >
               <Dialog.Panel className="relative flex w-full max-w-xs flex-col overflow-y-auto bg-white pb-12 shadow-xl">
                 <div className="flex px-4 pb-2 pt-5">
-                  <button
-                    type="button"
-                    className="relative -m-2 inline-flex items-center justify-center rounded-md p-2 text-gray-400"
-                    onClick={() => setOpen(false)}
-                  >
+                  <button type="button" className="relative -m-2 inline-flex items-center justify-center rounded-md p-2 text-gray-400" onClick={() => setOpen(false)} >
                     <span className="absolute -inset-0.5" />
                     <span className="sr-only">Close menu</span>
                     <XMarkIcon className="h-6 w-6" aria-hidden="true" />
@@ -288,43 +294,40 @@ export default function Navbar() {
           </div>
         </Dialog>
       </Transition.Root>
-
+      
+      {/* Dekstop */}
       <header className="relative bg-white">
         <p className="flex h-10 items-center justify-center bg-indigo-600 px-4 text-sm font-medium text-white sm:px-6 lg:px-8">
           Get free delivery on orders over $100
         </p>
 
-        <nav aria-label="Top" className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <nav aria-label="Top" className="mx-auto ">
           <div className="border-b border-gray-200">
-            <div className="flex h-16 items-center">
-              <button
-                type="button"
-                className="relative rounded-md bg-white p-2 text-gray-400 lg:hidden"
-                onClick={() => setOpen(true)}
-              >
-                <span className="absolute -inset-0.5" />
+            <div className="flex h-16 items-center px-11">
+              <button  type="button"  className="relative rounded-md bg-white p-2 text-gray-400 lg:hidden"  onClick={() => setOpen(true)}>
+                {/* <span className="absolute -inset-0.5" /> */}
                 <span className="sr-only">Open menu</span>
                 <Bars3Icon className="h-6 w-6" aria-hidden="true" />
               </button>
 
               {/* Logo */}
               <div className="ml-4 flex lg:ml-0">
-                <a href="#">
+                {/* <a href="#"> */}
                   <span className="sr-only">Your Company</span>
                   <img
-                    className="h-8 w-auto"
+                    className="h-8 w-8 mr-2"
                     src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                     alt=""
                   />
-                </a>
+                {/* </a> */}
               </div>
 
               {/* Flyout menus */}
-              <Popover.Group className="hidden lg:ml-8 lg:block lg:self-stretch">
+              <Popover.Group className="hidden lg:ml-8 lg:block lg:self-stretch z-10">
                 <div className="flex h-full space-x-8">
                   {navigation.categories.map((category) => (
                     <Popover key={category.name} className="flex">
-                      {({ open }) => (
+                      {({ open , close}) => (
                         <>
                           <div className="relative flex">
                             <Popover.Button
@@ -339,19 +342,10 @@ export default function Navbar() {
                             </Popover.Button>
                           </div>
 
-                          <Transition
-                            as={Fragment}
-                            enter="transition ease-out duration-200"
-                            enterFrom="opacity-0"
-                            enterTo="opacity-100"
-                            leave="transition ease-in duration-150"
-                            leaveFrom="opacity-100"
-                            leaveTo="opacity-0"
-                          >
+                          <Transition  as={Fragment}  enter="transition ease-out duration-200"  enterFrom="opacity-0"  enterTo="opacity-100"  leave="transition ease-in duration-150"  leaveFrom="opacity-100"  leaveTo="opacity-0">
                             <Popover.Panel className="absolute inset-x-0 top-full text-sm text-gray-500">
                               {/* Presentational element used to render the bottom shadow, if we put the shadow on the actual panel it pokes out the top, so we use this shorter element to hide the top of the shadow */}
-                              <div className="absolute inset-0 top-1/2 bg-white shadow" aria-hidden="true" />
-
+                              <div className="absolute inset-0 top-1/2 bg-white shadow" aria-hidden="true" /> 
                               <div className="relative bg-white">
                                 <div className="mx-auto max-w-7xl px-8">
                                   <div className="grid grid-cols-2 gap-x-8 gap-y-10 py-16">
@@ -359,11 +353,7 @@ export default function Navbar() {
                                       {category.featured.map((item) => (
                                         <div key={item.name} className="group relative text-base sm:text-sm">
                                           <div className="aspect-h-1 aspect-w-1 overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75">
-                                            <img
-                                              src={item.imageSrc}
-                                              alt={item.imageAlt}
-                                              className="object-cover object-center"
-                                            />
+                                            <img  src={item.imageSrc}  alt={item.imageAlt}  className="object-cover object-center"/>
                                           </div>
                                           <a href={item.href} className="mt-6 block font-medium text-gray-900">
                                             <span className="absolute inset-0 z-10" aria-hidden="true" />
@@ -381,16 +371,15 @@ export default function Navbar() {
                                           <p id={`${section.name}-heading`} className="font-medium text-gray-900">
                                             {section.name}
                                           </p>
-                                          <ul
-                                            role="list"
-                                            aria-labelledby={`${section.name}-heading`}
-                                            className="mt-6 space-y-6 sm:mt-4 sm:space-y-4"
-                                          >
+                                          <ul  role="list"  aria-labelledby={`${section.name}-heading`}  className="mt-6 space-y-6 sm:mt-4 sm:space-y-4">
                                             {section.items.map((item) => (
                                               <li key={item.name} className="flex">
-                                                <a href={item.href} className="hover:text-gray-800">
+                                                {/* <a href={item.href} className="hover:text-gray-800">
                                                   {item.name}
-                                                </a>
+                                                </a> */}
+                                                <p onClick={() => handleCategoryClick(category,section,item,close)} className='cursor-pointer hover:to-gray-800'>
+                                                  {item.name}
+                                                </p>
                                               </li>
                                             ))}
                                           </ul>
@@ -408,11 +397,7 @@ export default function Navbar() {
                   ))}
 
                   {navigation.pages.map((page) => (
-                    <a
-                      key={page.name}
-                      href={page.href}
-                      className="flex items-center text-sm font-medium text-gray-700 hover:text-gray-800"
-                    >
+                    <a key={page.name} href={page.href} className="flex items-center text-sm font-medium text-gray-700 hover:text-gray-800" >
                       {page.name}
                     </a>
                   ))}
@@ -421,7 +406,31 @@ export default function Navbar() {
 
               <div className="ml-auto flex items-center">
                 <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
-                  <a href="#" className="text-sm font-medium text-gray-700 hover:text-gray-800">
+                  {
+                    true ? (
+                      <div>
+                        <Avatar className='text-white' onClick={handleUserClick} aria-controls={open ? "basic-menu" : undefined} aria-haspopup="true" aria-expanded={open ? "true" : undefined} sx={{bgcolor:deepPurple[500], color:"white", cursor:"pointer",}}>
+                          A
+                        </Avatar>
+                        <Menu id='basic-menu' anchorEl={anchorEl} open={openUserMenu} onClose={handleCloseUserMenu} MenuListProps={{"aria-labelledby" : "basic-button"}}>
+                          <MenuItem onClick={handleCloseUserMenu}>
+                            Profile
+                          </MenuItem>
+                          <MenuItem>
+                            My Orders
+                          </MenuItem>
+                          <MenuItem>
+                            Logout
+                          </MenuItem>
+                        </Menu>
+                      </div>
+                    ) : (
+                      <Button onClick={handleOpen} className='text-sm font-medium text-gray-700 hover:text-gray-800'>
+                        Sign in
+                      </Button>
+                    )
+                  }
+                  {/* <a href="#" className="text-sm font-medium text-gray-700 hover:text-gray-800">
                     Sign in
                   </a>
                   <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
@@ -439,27 +448,28 @@ export default function Navbar() {
                     />
                     <span className="ml-3 block text-sm font-medium">CAD</span>
                     <span className="sr-only">, change currency</span>
-                  </a>
+                  </a> */}
                 </div>
 
                 {/* Search */}
                 <div className="flex lg:ml-6">
-                  <a href="#" className="p-2 text-gray-400 hover:text-gray-500">
+                  {/* <a href="#" className="p-2 text-gray-400 hover:text-gray-500"> */}
+                  <p className='p-2 text-gray-400 hover:text-gray-500'>
                     <span className="sr-only">Search</span>
                     <MagnifyingGlassIcon className="h-6 w-6" aria-hidden="true" />
-                  </a>
+                  </p>
+                  {/* </a> */}
                 </div>
 
                 {/* Cart */}
                 <div className="ml-4 flow-root lg:ml-6">
-                  <a href="#" className="group -m-2 flex items-center p-2">
-                    <ShoppingBagIcon
-                      className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
-                      aria-hidden="true"
-                    />
+                  {/* <a href="#" className="group -m-2 flex items-center p-2"> */}
+                  <Button className='group -m-2 items-center p-2'>
+                    <ShoppingBagIcon className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500" aria-hidden="true" />
                     <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">0</span>
                     <span className="sr-only">items in cart, view bag</span>
-                  </a>
+                  </Button>
+                  {/* </a> */}
                 </div>
               </div>
             </div>
