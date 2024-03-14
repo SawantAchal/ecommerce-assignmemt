52b38ba2cb09c8701e2eec70eab407af287e712c
SawantAchal <achals1718@gmail.com>
2024-03-14 21:24:42 +0530

    Add border

--- src/customer/component/homeSectionCarousel/HomeSectionCarousel.jsx
+++ src/customer/component/homeSectionCarousel/HomeSectionCarousel.jsx
@@ -5,20 +5,15 @@ import HomeCard from '../homeCard/HomeCard'
 import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 import { Button } from '@mui/material';
 
-
-
-
-
-
-const HomeSectionCarousel = () => {
+const HomeSectionCarousel = ({data ,sectionName}) => {
     const [activeIndex , setActiveIndex] = useState(0)
     const responsive = {
         0: { items: 1 },
         720: { items: 3 },
-        1024: { items: 5.5},
+        1024: { items: 6},
     };
 
-    const items = [1,1,1,1,1,1,1,1,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 ].map((item) => <HomeCard />)
+    const items = data.slice(0,12).map((item) => <HomeCard product={item}/>)
 
     const slidePrev = () => setActiveIndex(activeIndex - 1);
     const slideNext = () => setActiveIndex(activeIndex + 1);
@@ -27,9 +22,11 @@ const HomeSectionCarousel = () => {
 
   return (
     <>
-        <div className='relative px-4 lg:px-8 w-full border-yellow-950 border-solid'>
+        {/* <div className='relative px-4 lg:px-8 w-full border-yellow-950 border-solid'> */}
+        <div className='border'>
+            <h2 className='text-2xl font-extrabold text-gray-800 '>{sectionName}</h2>
             <div className='relative p-5 '>
-                <AliceCarousel items={items} disableButtonsControls responsive={responsive} infinite disableDotsControls onSlideChanged={syncActiveIndex} activeIndex={activeIndex}/>
+                <AliceCarousel items={items} disableButtonsControls responsive={responsive}  disableDotsControls onSlideChanged={syncActiveIndex} activeIndex={activeIndex}/>
                 {
                     activeIndex !== items.length - 5 &&
                     <Button onClick={slideNext} className='z-50 bg-white' sx={{position:'absolute' ,top:'8rem' , right:'0rem' , transform:'translateX(50%) rotate(90deg)' , bgcolor:"white"}} variant='contained' aria-label='next'>
